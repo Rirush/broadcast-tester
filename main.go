e4b1dd21b6e76b3c594cae3803c03d7853e6e7fd
Nick Rirush <RaccoonRirush@gmail.com>
2020-06-24 12:23:29 +0800

    Add UDP server
    * mclient/uclient are now just client because their code is identical
    * userver now works

--- main.go
+++ main.go
@@ -9,9 +9,8 @@ import (
 
 const Usage = `Usage:
   broadcast-tester mserver ADDRESS PORT MESSAGE - start multicast server at ADDRESS:PORT and respond to all packets with MESSAGE
-  broadcast-tester mclient ADDRESS PORT MESSAGE - send packet to multicast server at ADDRESS:PORT with MESSAGE
   broadcast-tester userver ADDRESS PORT MESSAGE - start UDP server at ADDRESS:PORT and respond to all packets with MESSAGE
-  broadcast-tester uclient ADDRESS PORT MESSAGE - send packet to UDP server at ADDRESS:PORT with MESSAGE`
+  broadcast-tester client ADDRESS PORT MESSAGE - send packet to UDP server at ADDRESS:PORT with MESSAGE`
 
 func main() {
 	if len(os.Args) != 5 {
@@ -21,12 +20,10 @@ func main() {
 	switch os.Args[1] {
 	case "mserver":
 		mserver()
-	case "mclient":
-		mclient()
 	case "userver":
 		userver()
-	case "uclient":
-		uclient()
+	case "client":
+		client()
 	default:
 		fmt.Println(Usage)
 		return
@@ -58,7 +55,7 @@ func mserver() {
 	}
 }
 
-func mclient() {
+func client() {
 	address, port, message := os.Args[2], os.Args[3], os.Args[4]
 	addr, err := net.ResolveUDPAddr("udp", address + ":" + port)
 	if err != nil {
@@ -81,9 +78,26 @@ func mclient() {
 }
 
 func userver() {
-
-}
-
-func uclient() {
-
+	address, port, message := os.Args[2], os.Args[3], os.Args[4]
+	addr, err := net.ResolveUDPAddr("udp", address + ":" + port)
+	if err != nil {
+		log.Fatal("Cannot resolve address:", err)
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatal("Cannot dial specified address:", err)
+	}
+	log.Println("Listening for UDP packets at", address + ":" + port)
+	for {
+		b := make([]byte, 1024)
+		n, src, err := conn.ReadFrom(b)
+		if err != nil {
+			log.Fatal("Cannot read from UDP:", err)
+		}
+		log.Println("Received a message from", src, "with content", string(b[:n]))
+		_, err = conn.WriteTo([]byte(message), src)
+		if err != nil {
+			log.Fatal("Cannot respond to packet:", err)
+		}
+	}
 }
